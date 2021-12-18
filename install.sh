ba195441881dbf737a9809cf86ea0523eae732fb
Sasha Melentyev <alexander@melentyev.org>
2021-12-18 12:45:51 +0300

    Add git installation and rename functions

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-install_zsh() {
+git() {
+	echo "Installing .gitconfig"
+	rm -rf ~/.gitconfig
+	ln -s "$PWD"/.gitconfig ~/.gitconfig
+}
+
+zsh() {
 	echo "Installing .zshrc"
 	rm -rf ~/.zshrc
 	ln -s "$PWD"/.zshrc ~/.zshrc
