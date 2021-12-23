611b868b98b54fb3d7c9119c54f18a860132b3fc
Sasha Melentyev <sasha@melentyev.io>
2021-12-23 18:40:27 +0300

    refactor(install): remove brew

--- install.sh
+++ install.sh
@@ -20,12 +20,6 @@ case $MACHINE in
 
     xcode-select --install
 
-    if ! [ -e /opt/homebrew/bin/brew ]; then
-      /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    else
-      echo "Homebrew already installed"
-    fi
-
     if ! [ -e ~/.oh-my-zsh ]; then
       curl -Lo ohmyzsh.sh https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh;
       sed -i "" "/exec zsh -l/d" ohmyzsh.sh
