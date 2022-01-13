bfe551c11336d732e8760659672f99c4cf19994e
Sasha Melentyev <sasha@melentyev.io>
2022-01-13 21:12:09 +0300

    feat(install): add sudo for shfmt install

--- install.sh
+++ install.sh
@@ -44,6 +44,6 @@ go install entgo.io/ent/cmd/ent@latest
 go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
 go install github.com/bufbuild/buf/cmd/buf@latest
 go install github.com/go-dummy/dummy/cmd/dummy@latest
-go install mvdan.cc/sh/v3/cmd/shfmt@latest
+sudo go install mvdan.cc/sh/v3/cmd/shfmt@latest
 go install github.com/thepudds/fzgen/cmd/fzgen@latest
 go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
