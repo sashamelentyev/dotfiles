223093ba44ddf8f6b142be72d5464cc820ad6ddb
Sasha Melentyev <sasha@melentyev.io>
2022-03-12 20:26:09 +0300

    remove desktop applications

--- linux.sh
+++ linux.sh
@@ -11,8 +11,4 @@ sudo apt install snapd
 
 sudo snap install nvim --classic
 sudo snap install go --classic
-sudo snap install telegram-desktop
-sudo snap install slack --classic
 sudo snap install tmux --classic
-sudo snap install zoom-client
-sudo snap install obs-studio
