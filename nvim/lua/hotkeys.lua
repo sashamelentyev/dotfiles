20d35faac2e461708698e3f0b452be0adb5186ad
Sasha Melentyev <sasha@melentyev.io>
2022-03-18 18:50:27 +0300

    Изменено описание

--- nvim/lua/hotkeys.lua
+++ nvim/lua/hotkeys.lua
@@ -1,7 +1,7 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = {noremap = true, silent = false}
 
-keymap('', '<up>', ':echoe "Use K"<CR>', opts)
-keymap('', '<down>', ':echoe "Use J"<CR>', opts)
-keymap('', '<left>', ':echoe "Use H"<CR>', opts)
-keymap('', '<right>', ':echoe "Use L"<CR>', opts)
+keymap('', '<up>', ':echoe "Используй клавишу K"<CR>', opts)
+keymap('', '<down>', ':echoe "Используй клавишу J"<CR>', opts)
+keymap('', '<left>', ':echoe "Используй клавишу H"<CR>', opts)
+keymap('', '<right>', ':echoe "Используй клавишу L"<CR>', opts)
