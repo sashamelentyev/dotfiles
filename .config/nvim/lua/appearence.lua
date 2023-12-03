d668505c404565afc5c654b004b99b834c0230c0
Sasha Melentyev <sasha@m8.ru>
2023-12-03 21:16:52 +0300

    feat(vim): add more colors
    Signed-off-by: Sasha Melentyev <sasha@m8.ru>

--- .config/nvim/lua/appearence.lua
+++ .config/nvim/lua/appearence.lua
@@ -1,7 +1,5 @@
 local cmd = vim.cmd
 
-local colors = require("colors")
-
 local function bg(group, color)
     cmd("hi " .. group .. " guibg=" .. color)
 end
@@ -10,10 +8,22 @@ local function fg(group, color)
     cmd("hi " .. group .. " guifg=" .. color)
 end
 
+local colors = require("colors")
+
 bg("Normal", colors.black) -- backgroud
-fg("Normal", colors.white)
+fg("Normal", colors.white) -- foreground
 fg("LineNr", colors.gray)
 fg("Comment", colors.gray)
 bg("Todo", colors.red)
 fg("Statement", colors.yellow)
 fg("Function", colors.yellow)
+fg("Constant", colors.red)
+fg("String", colors.green)
+fg("Conditional", colors.yellow)
+fg("Repeat", colors.yellow)
+bg("Label", colors.red)
+bg("Operator", colors.red)
+fg("Keyword", colors.yellow)
+bg("Exception", colors.red)
+fg("Type", colors.yellow)
+
--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,7 +1,8 @@
 local colors = {
     black = "#221D22",
     gray = "#312D31",
-    red = "#E8476D",
+    green = "#3BBC50",
+    red = "#FF5958",
     yellow = "#FFBE5C",
     white = "#FFFFFFb3",
 }
